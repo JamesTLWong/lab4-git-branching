35af00c12fce65b1cf0377ce3075c3e59bdc94ed
James Wong <90537826+JamesTLWong@users.noreply.github.com>
2025-09-18 13:44:44 -0400

    Added prints: Finished writing hero/cursed sword path

--- story.py
+++ story.py
@@ -13,6 +13,9 @@ def intro():
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
     print("You call up a local wizard to inspect the obviously magical sword, because you have no idea what kind of magics could be at work.")
+    print("The wizard tells you that the sword was very cursed, but they have removed the curse and now you are left a sword that would fetch quite the price with collectors")
+    print("You end up selling the sword for a high price, leaving you and your family very well off.")
+    print("You have ensured good lives for you and your family while also ridding the world of a cursed sword")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
